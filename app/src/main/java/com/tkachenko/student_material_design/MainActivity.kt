e07ec130bd287a06272b987135a77170b13ec4fa
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-03-18 15:15:01 +0300

    Step 11: Animation and expand/collapse functionality added

--- app/src/main/java/com/tkachenko/student_material_design/MainActivity.kt
+++ app/src/main/java/com/tkachenko/student_material_design/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tkachenko.student_material_design
 
+import android.content.ClipDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.tkachenko.student_material_design.data.Student
@@ -7,18 +8,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -34,6 +42,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.tkachenko.student_material_design.data.students
 import com.tkachenko.student_material_design.ui.theme.Student_Material_DesignTheme
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -89,24 +106,69 @@ fun StudentItem(
     student: Student,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
     Card(modifier = modifier) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small))
 
-        ){
-            StudentIcon(student.imageResourceId)
-            StudentInformation(student.name, student.age)
+            ) {
+                StudentIcon(student.imageResourceId)
+                StudentInformation(student.name, student.age)
+                Spacer(modifier = Modifier.weight(1f))
+                StudentItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded }
+                )
+            }
+            if (expanded){
+                StudentDescription(
+                    student.description,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_small),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium),
+                    )
+                )
+            }
         }
+    }
+}
+@Composable
+private fun StudentItemButton(
+    expanded:Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled
+                .ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
     }
 }
 @Composable
 fun StudentApp(){
     Scaffold(
         topBar = {StudentTopAppBar()}
-    ) {it -> LazyColumn(contentPadding = it) { }
-    LazyColumn {
+    ) {it -> LazyColumn(contentPadding = it) {
+//    LazyColumn {
         items(students){
             StudentItem(student = it,
                 modifier = Modifier
@@ -151,4 +213,23 @@ fun Studenrprew(){
 @Composable
 fun StudentdarkthemePreview(){
     Student_Material_DesignTheme(darkTheme = true) { StudentApp()}
+}
+@Composable
+
+fun StudentDescription(
+    @StringRes studentDescription: Int,
+    modifier: Modifier= Modifier
+){
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = stringResource(studentDescription),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
 }
